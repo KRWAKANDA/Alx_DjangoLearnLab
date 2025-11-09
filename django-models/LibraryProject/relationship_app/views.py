1675fe2bbeb5b7cd25127259f7b22aced98b79d5
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-09 14:50:50 +0200

    Fix import statement for DetailView in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library  
 from .models import Library
-
+from django.views.generic.detail import DetailView
 
 # -------------------------
 # Function-Based View
@@ -20,6 +20,7 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
 
 
 
