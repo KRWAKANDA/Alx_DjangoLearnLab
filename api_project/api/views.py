519175a7f16e5fd3608dac6802e54711326910b0
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-19 17:24:42 +0200

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,23 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics, viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides CRUD operations for the Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
