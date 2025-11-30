834a30fa0e7423b5110fdc6d6a4aa9b42ef0d6a9
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-11-30 12:38:43 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 
 
 
@@ -103,5 +104,6 @@ class BookListView(generics.ListAPIView):
 
     # Optional: default ordering
     ordering = ['title']
+
 
 
