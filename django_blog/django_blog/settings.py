681a762191b3b8764d2c4f3344ada7c6a6cb0e0c
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-12-05 12:20:27 +0200

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'your_username',     
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',                
     }
 }
+
 
 
 
@@ -146,5 +151,6 @@ TEMPLATES = [
         },
     },
 ]
+
 
 
