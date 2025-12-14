8fbf0d05e41000f3f954b636975c50b92bfc1cc9
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-12-14 09:36:30 +0200

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    post = models.ForeignKey(
+        "Post",
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
+
 
 
 class Post(models.Model):
@@ -34,3 +57,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
